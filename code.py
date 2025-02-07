db9c394d59d3b9aa246f871b3102b774d124c859
aviralgit1 <Lionsdav1@gmail.com>
2025-02-07 12:10:39 +0530

    Update code.py

--- code.py
+++ code.py
@@ -69,7 +69,7 @@ export default WeatherApp;
 /*
 MIT License
 
-Copyright (c) 2025 Your Name
+Copyright (c) 2025 aviralgit1
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
